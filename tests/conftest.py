65a5013f9e26333cdb9a0f4b9f028060679c0200
Greg Wilson <gvwilson@third-bit.com>
2025-08-05 19:07:58 -0400

    feat: refactor tests and add one
    -   Move fixtures to `conftest.py`.
    -   Add test for building a single Markdown file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,39 @@
 """Test configuration and fixtures."""
+
+from argparse import Namespace
+
+import pytest
+
+CONFIG = """\
+[tool.mccole]
+skips = []
+"""
+
+TEMPLATE = """\
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+{{content}}
+  </body>
+</html>
+"""
+
+
+@pytest.fixture
+def bare_fs(tmp_path):
+    (tmp_path / "templates").mkdir()
+    (tmp_path / "templates" / "page.html").write_text(TEMPLATE)
+    (tmp_path / "pyproject.toml").write_text(CONFIG)
+    return tmp_path
+
+
+@pytest.fixture
+def opt(bare_fs):
+    return Namespace(
+        config=bare_fs / "pyproject.toml",
+        dst=bare_fs / "docs",
+        src=bare_fs,
+        templates=bare_fs / "templates",
+    )
--- tests/test_build.py
+++ tests/test_build.py
@@ -1,35 +1,22 @@
 """Test that sites build."""
 
-from argparse import Namespace
-from pathlib import Path
-
-import pytest
+from bs4 import BeautifulSoup
 
 from mccole.build import build
 
 
-TEMPLATE = """\
-<html>
-  <head>
-    <title></title>
-  </head>
-  <body>
-{{content}}
-  </body>
-</html>
-"""
-
-
-@pytest.fixture
-def bare_fs(tmp_path):
-    (tmp_path / "src").mkdir()
-    (tmp_path / "templates").mkdir()
-    (tmp_path / "templates" / "page.html").write_text(TEMPLATE)
-    return tmp_path
-
-
-def test_build_with_no_files_does_not_create_output_directory(bare_fs):
-    dst = bare_fs / "docs"
-    opt = Namespace(dst=dst, src=bare_fs / "src", templates=bare_fs / "templates")
+def test_build_with_no_files_does_not_create_output_directory(bare_fs, opt):
     build(opt)
+    dst = bare_fs / opt.dst
     assert not dst.exists()
+
+
+def test_build_with_single_plain_markdown_file_creates_one_output_file(bare_fs, opt):
+    (bare_fs / "test.md").write_text("# Title\nbody")
+    build(opt)
+    expected = bare_fs / opt.dst / "test.html"
+    assert expected.is_file()
+    doc = BeautifulSoup(expected.read_text(), "html.parser")
+    assert doc.title.string == "Title"
+    paragraphs = doc.find_all("p")
+    assert len(paragraphs) == 1 and paragraphs[0].string == "body"
